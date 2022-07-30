47805ba634584051dd4fe74df5e0b71ab93f0311
Sule_jr92 <Sule_jr92@STHQ0129-05>
2022-07-30 13:00:26 +0400

    Add project files.

---
+++ UC/Details.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace UC
+{
+    partial class Details
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.namelbl = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.pricelbl = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(3, 24);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(213, 233);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox1.TabIndex = 0;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(216, 75);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(154, 32);
+            this.guna2HtmlLabel1.TabIndex = 1;
+            this.guna2HtmlLabel1.Text = "Product Name:";
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(216, 123);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(147, 32);
+            this.guna2HtmlLabel2.TabIndex = 2;
+            this.guna2HtmlLabel2.Text = "Product Price:";
+            // 
+            // namelbl
+            // 
+            this.namelbl.BackColor = System.Drawing.Color.Transparent;
+            this.namelbl.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.namelbl.Location = new System.Drawing.Point(376, 75);
+            this.namelbl.Name = "namelbl";
+            this.namelbl.Size = new System.Drawing.Size(78, 32);
+            this.namelbl.TabIndex = 3;
+            this.namelbl.Text = "namelbl";
+            // 
+            // pricelbl
+            // 
+            this.pricelbl.BackColor = System.Drawing.Color.Transparent;
+            this.pricelbl.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.pricelbl.Location = new System.Drawing.Point(376, 123);
+            this.pricelbl.Name = "pricelbl";
+            this.pricelbl.Size = new System.Drawing.Size(176, 32);
+            this.pricelbl.TabIndex = 4;
+            this.pricelbl.Text = "guna2HtmlLabel3";
+            // 
+            // Details
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SpringGreen;
+            this.Controls.Add(this.pricelbl);
+            this.Controls.Add(this.namelbl);
+            this.Controls.Add(this.guna2HtmlLabel2);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.Name = "Details";
+            this.Size = new System.Drawing.Size(586, 298);
+            this.Load += new System.EventHandler(this.Details_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel namelbl;
+        private Guna.UI2.WinForms.Guna2HtmlLabel pricelbl;
+    }
+}
---
+++ UC/Details.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UC
+{
+    public partial class Details : UserControl
+    {
+
+        public string Name
+        {
+            get => namelbl.Text;
+            set
+            {
+                namelbl.Text = value;
+            }
+        }
+
+        public double Price
+        {
+            get => double.Parse(pricelbl.Text);
+            set
+            {
+                pricelbl.Text = value.ToString();
+            }
+        }
+
+        public Image ProductImage
+        {
+            get
+            {
+                return guna2PictureBox1.Image;
+            }
+            set
+            {
+                guna2PictureBox1.Image = value;
+            }
+        }
+
+        public Details()
+        {
+            InitializeComponent();
+        }
+
+        private void Details_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
